62c730a1dfe5e99aa93c58f6050e44df6e12b2dd
Scott Rapson <scottapotamas@gmail.com>
2021-01-14 21:49:27 +1030

    Simple arduino usage example sketch
    Bare minimum for usage

---
+++ examples/basics/basics.ino
@@ -0,0 +1,113 @@
+// This example uses 'Serial' which maps to hardware serial on an Uno
+// For other hardware targets, modify the serial usage as needed
+
+#include "xsens_mti.h"      // main library
+#include "xsens_utility.h"  // needed for quaternion conversion function
+
+// Cache a copy of IMU data
+float    temperature     = 0;       // in degress celcius
+uint32_t pressure        = 0;       // in pascals
+float    euler_pry[3]    = { 0 };   // -180 to +180 degress
+float    acceleration[3] = { 0 };   // in m/s^2
+
+// Callback functions used by the library
+void imu_callback( XsensEventFlag_t event, XsensEventData_t *mtdata );
+void imu_send_data( uint8_t *data, uint16_t length );
+
+// The library holds it's state and pointers to callbacks in this structure 
+xsens_interface_t imu_interface = { 
+    .event_cb = &imu_callback, 
+    .output_cb = &imu_send_data 
+};
+
+// Normal arduino setup/loop functions
+void setup( void )
+{
+    Serial.begin( 115200 );
+    pinMode( LED_BUILTIN, OUTPUT );
+}
+
+void loop( void )
+{
+    // Inbound serial data needs to be parsed
+    // When a valid packet is decoded, imu_callback() will fire
+    while( Serial.available() > 0 )  
+    {  
+        xsens_mti_parse( &imu_interface, Serial.read() );
+    }
+
+    // Light goes high if the IMU pitch exceeds 10 degrees
+    digitalWrite( LED_BUILTIN, (euler_pry[0] > 10.0f) );
+}
+
+
+// Called when the library decoded an inbound packet
+//
+// If the packet was an MData2 frame (which contains packed motion data)
+// the callback is called once for each sub-field
+void imu_callback( XsensEventFlag_t event, XsensEventData_t *mtdata )
+{
+    // The library provides a pointer to the a union containing decoded data
+    // Use XsensEventFlag_t determine what kind of packet arrived, 
+    // then copy data from the union as needed
+
+    // union
+    // {
+    //     uint8_t u1;
+    //     uint16_t u2;
+    //     uint32_t u4;
+    //     float    f4;
+    //     float    f4x2[2];
+    //     float    f4x3[3];
+    //     float    f4x4[4];
+    //     float    f4x9[9];
+    // } data;
+
+    switch( event )
+    {
+        case XSENS_EVT_QUATERNION:
+            // We use the type field of XsensEventData_t as a doublecheck before
+            // blindly copying bytes out of the union
+
+            if( mtdata->type == XSENS_EVT_TYPE_FLOAT4 )
+            {
+                // Convert the quaternion to euler angles
+                xsens_quaternion_to_euler( mtdata->data.f4x4, euler_pry );
+
+                // Convert from radians to degrees
+                euler_pry[0] *= (180.0 / PI);
+                euler_pry[1] *= (180.0 / PI);
+                euler_pry[2] *= (180.0 / PI);
+            }
+            break;
+
+        case XSENS_EVT_ACCELERATION:
+            if( mtdata->type == XSENS_EVT_TYPE_FLOAT3 )
+            {
+                acceleration[0] = mtdata->data.f4x3[0];
+                acceleration[1] = mtdata->data.f4x3[1];
+                acceleration[2] = mtdata->data.f4x3[2];
+            }
+            break;
+
+        case XSENS_EVT_PRESSURE:
+            if( mtdata->type == XSENS_EVT_TYPE_U32 )
+            {
+                pressure = mtdata->data.u4;
+            }
+            break;
+
+        case XSENS_EVT_TEMPERATURE:
+            if( mtdata->type == XSENS_EVT_TYPE_FLOAT )
+            {
+                temperature = mtdata->data.f4;
+            }
+            break;
+    }
+}
+
+// The library calls this function to send packets to the IMU
+void imu_send_data( uint8_t *data, uint16_t length )
+{
+    Serial.write( data, len );
+}
