34cbec8607d31e3901d2153a63654a5b672285c2
Scott Rapson <scottapotamas@gmail.com>
2022-09-13 15:17:43 +0930

    Tests for packet handle table functions

--- src/xsens_mti_private.h
+++ src/xsens_mti_private.h
@@ -21,10 +21,13 @@ void xsens_internal_handle_mdata2( xsens_packet_buffer_t *packet );
 // after a valid packet is parsed
 void xsens_mti_handle_payload( xsens_interface_t *interface );
 
-// Search the message handler table for a given ID
+// Search the hardcoded message handler table for a given ID
 // Returns a pointer to the entry if it exists
 // Returns 0 if no match is found
 message_handler_ref_t *xsens_mti_find_inbound_handler_entry( uint8_t find_id );
+
+// Testable find-in-table function used by xsens_mti_find_inbound_handler_entry
+message_handler_ref_t *xsens_mti_find_handler_entry( uint8_t find_id, message_handler_ref_t *entry_table, uint8_t entry_num );
 
 // Run through a buffer of bytes and return the CRC
 // Used when sending messages
